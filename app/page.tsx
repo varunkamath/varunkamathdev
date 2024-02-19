df0e11d9c87a7232f80e5b0b70e9954feacf3ca2
Varun Kamath <varun.kamath@gmail.com>
2024-02-19 13:56:28 -0600

    Messy rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,201 @@
 'use client'
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Head from 'next/head';
 import Typewriter from 'typewriter-effect';
+import * as THREE from 'three';
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+import { WebGLRenderer } from "three/src/Three.js";
+
+export function CubeComponent() {
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.y = 15;
+    camera.position.z = 100;
+    camera.rotation.z = 4;
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+
+    let effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: true });
+    effect.setSize(window.innerWidth, window.innerHeight);
+    effect.domElement.style.color = 'white';
+    effect.domElement.style.backgroundColor = 'black';
+
+    let controls = new TrackballControls(camera, effect.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // const material = new THREE.MeshPhongMaterial({ flatShading: true })
+    // const cube = new THREE.Mesh(geometry, material);
+    // scene.add(cube);
+    let cube = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshPhongMaterial({ flatShading: true }));
+    scene.add(cube);
+
+    const pointLight1 = new THREE.PointLight(0xffffff, 3, 0, 0);
+    pointLight1.position.set(500, 500, 500);
+    scene.add(pointLight1);
+
+    const pointLight2 = new THREE.PointLight(0xffffff, 1, 0, 0);
+    pointLight2.position.set(- 500, - 500, - 500);
+    scene.add(pointLight2);
+
+    camera.position.z = 5;
+
+    const animate = function () {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      // renderer.render(scene, camera);
+      controls.update();
+      effect.render(scene, camera);
+    };
+
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    if (mountRef.current) {
+      mountRef.current.appendChild(effect.domElement);
+    }
+
+    animate();
+
+    // Clean up on unmount
+    return () => {
+      if (mountRef.current) {
+        mountRef.current.removeChild(effect.domElement);
+      }
+    };
+  }, []);
+
+  return (<div id="canvas" ref={mountRef} className="w-1/2 p-4 border">
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <Typewriter
+        options={{
+          cursor: "_",
+          delay: 100,
+        }}
+        onInit={(typewriter) => {
+          typewriter.pauseFor(2000).typeString('👋, I\'m <a href="https://varunkamath.dev" class="underline">Varun</a>.<br /><br /><a href="https://github.com/varunkamath" class="underline">github.com/varunkamath</a><br /><br /><a href="https://linkedin.com/in/varun-kamath" class="underline">linkedin.com/in/varun-kamath</a><br /><br />')
+            .callFunction(() => {
+              console.log('String typed out!');
+            })
+            .start();
+        }}
+      />
+    </div>
+  </div>);
+}
+
+export function AsciiBall() {
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let camera: THREE.PerspectiveCamera, controls: TrackballControls, scene: THREE.Scene, renderer: WebGLRenderer, effect: AsciiEffect;
+    let sphere: THREE.Mesh, plane: THREE.Mesh;
+    const start = Date.now();
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.y = 150;
+    camera.position.z = 500;
+    camera.rotation.z = 4;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0, 0, 0);
+
+    const pointLight1 = new THREE.PointLight(0xffffff, 3, 0, 0);
+    pointLight1.position.set(500, 500, 500);
+    scene.add(pointLight1);
+
+    const pointLight2 = new THREE.PointLight(0xffffff, 1, 0, 0);
+    pointLight2.position.set(- 500, - 500, - 500);
+    scene.add(pointLight2);
+
+    sphere = new THREE.Mesh(new THREE.SphereGeometry(200, 20, 10), new THREE.MeshPhongMaterial({ flatShading: true }));
+    scene.add(sphere);
+
+    // Plane
+
+    plane = new THREE.Mesh(new THREE.PlaneGeometry(400, 400), new THREE.MeshBasicMaterial({ color: 0xe0e0e0 }));
+    plane.position.y = - 200;
+    plane.rotation.x = - Math.PI / 2;
+    plane.rotation.z = Math.PI / 3;
+    scene.add(plane);
+
+    renderer = new THREE.WebGLRenderer();
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: true });
+    effect.setSize(window.innerWidth, window.innerHeight);
+    // effect.setSize(400, 400);
+    effect.domElement.style.color = 'white';
+    effect.domElement.style.backgroundColor = 'black';
+
+    // Special case: append effect.domElement, instead of renderer.domElement.
+    // AsciiEffect creates a custom domElement (a div container) where the ASCII elements are placed.
+
+    // document.body.appendChild(effect.domElement);
+
+    // controls = new TrackballControls(camera, effect.domElement);
+
+    // Add Typewriter with text in the center of the effect.domElement
+
+    //
+
+    window.addEventListener('resize', onWindowResize);
+
+    function onWindowResize() {
+
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      effect.setSize(window.innerWidth, window.innerHeight);
+
+    }
+    //
+
+    function animate() {
+
+      requestAnimationFrame(animate);
+      const timer = Date.now() - start;
+
+      sphere.position.y = Math.abs(Math.sin(timer * 0.002)) * 100;
+      sphere.rotation.x = timer * 0.0003;
+      sphere.rotation.z = timer * 0.0002;
+
+      controls.update();
+
+      effect.render(scene, camera);
+    }
+
+    if (mountRef.current) {
+      mountRef.current.appendChild(effect.domElement);
+    }
+
+    animate();
+    // Clean up on unmount
+    return () => {
+      if (mountRef.current) {
+        mountRef.current.removeChild(effect.domElement);
+      }
+    };
+  }, []);
+
+  return <div ref={mountRef} />;
+}
+
+// export function CubeComponent;
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mt-5">
-        <Typewriter
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 border">
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"> */}
+      {/* <CubeComponent /> */}
+      {/* <Typewriter
           options={{
             cursor: " ",
             delay: 10,
@@ -18,15 +206,17 @@ export default function Home() {
             })
               .start();
           }}
-        />
-      </div>
-      {/* <div className="z-11 max-w-5xl w-full items-center justify-between font-mono text-md md:flex"> */}
-      <div className="relative before:absolute max-w-5xl w-full max-h-1xl items-center font-mono text-md md:flex mb-28">
+        /> */}
+      {/* <AsciiBall /> */}
+      <CubeComponent />
+      {/* </div> */}
+      {/* <div className="relative before:absolute max-w-5xl w-full max-h-1xl items-center font-mono text-md md:flex mb-28"> */}
+      {/* <div className="z-100 smax-w-5xl w-full max-h-1xl items-center font-mono text-md md:flex mb-28">
         <Head>
           <title>Varun Kamath</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <Typewriter
+        {/* <Typewriter
           options={{
             cursor: "_",
             delay: 100,
@@ -38,8 +228,8 @@ export default function Home() {
               })
               .start();
           }}
-        />
-      </div>
+        /> */}
+      {/* </div> */}
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
