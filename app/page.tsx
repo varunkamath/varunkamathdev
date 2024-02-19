d9c05d9ef16b907c5d67bc1f7723135b71900c11
Varun Kamath <varun.kamath@gmail.com>
2024-02-18 21:07:29 -0600

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#000',
+        color: '#fff',
+        fontFamily: 'Lucida Console, Monaco, monospace',
+        fontSize: '2rem',
+      }}>
+        <Head>
+          <title>Varun Kamath</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        Varun Kamath
+      </div>
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
@@ -107,7 +124,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
